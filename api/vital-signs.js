a9f414184fdebb210fd8d383a5f0ee11b9d0718b
Bryson Palmer <brysonpalmer@gmail.com>
2024-11-04 15:21:35 -0800

    Switching from playwright to puppeteer core and @sparticuz/chromium

--- api/vital-signs.js
+++ api/vital-signs.js
@@ -1,10 +1,29 @@
 import * as cheerio from 'cheerio'
-import chromium from 'playwright-aws-lambda'
+import puppeteer from 'puppeteer'
+import puppeteerCore from 'puppeteer-core'
+import chromium from '@sparticuz/chromium'
 
 async function vitalSigns(req, res) {
   try {
     const urlBase = 'https://climate.nasa.gov'
-    const browser = await chromium.launchChromium({ headless: true })
+
+    let browser
+    if (process.env.VERCEL_ENV === 'production') {
+      const executablePath = await chromium.executablePath()
+      browser = await puppeteerCore.launch({
+        executablePath,
+        args: chromium.args,
+        headless: chromium.headless,
+        defaultViewport: chromium.defaultViewport
+      })
+    } else {
+      browser = await puppeteer.launch({
+        headless: 'new',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      })
+    }
+
+    // const browser = await chromium.launchChromium({ headless: true })
     const page = await browser.newPage()
     await page.goto('https://climate.nasa.gov/module/5/')
     const html = await page.content()
@@ -39,7 +58,10 @@ async function vitalSigns(req, res) {
         error: null,
         timestamp: new Date().toISOString(),
       })
-    await browser.close()
+
+    if (process.env.VERCEL_ENV !== 'production') {
+      await browser.close()
+    }
   } catch (error) {
     console.error('Error in scraping function:', error)
     res.status(500).json({ error: 'Failed to scrape vital signs' })
