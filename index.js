903b5b5f5aa2e4672d0a51d5f1a98ab74e042e34
Bryson Palmer <brysonpalmer@gmail.com>
2024-11-03 16:59:06 -0800

    Reconfigured express index to handle static files.

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import express from 'express'
-import * as api from './api/index.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import climateStories from './api/climate-stories.js'
+import vitalSigns from './api/vital-signs.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
-app.get('/', () => "Welcome to the NASA climate change api. Scraped with care by me, for you.")
-app.get('/api/climate-stories', api.climateStories)
-app.get('/api/vital-signs', api.vitalSigns)
+// Serve static files if applicable
+app.use(express.static(path.join(__dirname, 'public')))
 
-// Start the server locally (for development)
-const PORT = process.env.PORT || 8000
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 8000
+
+  app.get('/api/climate-stories', climateStories)
+  app.get('/api/vital-signs', vitalSigns)
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+  })
+}
 
 export default app
