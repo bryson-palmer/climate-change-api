9d1bc60b6bc9c93c43f322064b03834c17025b4c
Bryson Palmer <brysonpalmer@gmail.com>
2024-11-04 16:22:51 -0800

    Reconfiguring express server and vercel settings.

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ const app = express()
 // Serve static files if applicable
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Serve API routes for local development
+app.get('/api/climate-stories', climateStories)
+app.get('/api/vital-signs', vitalSigns)
+
+// Serve index.html for all other routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000
 
-  app.get('/api/climate-stories', climateStories)
-  app.get('/api/vital-signs', vitalSigns)
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-  })
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
 
 export default app
